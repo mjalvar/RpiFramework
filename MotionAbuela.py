b7d1c99e7f6d8d6bdeb5a200c87ca301fe741b7c
Melvin Alvarado <mjalvar@localhost>
2014-12-19 10:31:32 -0600

    Arreglo en logging MotionAbuela.

--- MotionAbuela.py
+++ MotionAbuela.py
@@ -51,7 +51,7 @@ class MotionAbuela:
 		self.controls.photo()
 
 		Cmd = Config.FRAMEWORK_PATH + '/run_client.py ' + config.get('ABUELA_BEEP_HOST') + ' ' + str(config.get('ABUELA_BEEP_PORT')) + ' ' + 'beep'
-		logging.info( 'Beep Cmd:',Cmd )
+		logging.info( 'Beep Cmd: ' + Cmd )
 		os.system(Cmd)
 
 
